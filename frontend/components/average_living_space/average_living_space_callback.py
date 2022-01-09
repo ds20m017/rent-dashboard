7cfae6fb1fc272269338ae3afd2b6a2bd2d9241d
Unknown <luo14938@gmail.com>
2022-01-09 15:18:06 +0100

    Moving average living space  layout and callback to own component folder

---
+++ frontend/components/average_living_space/average_living_space_callback.py
@@ -0,0 +1,47 @@
+from dash.dependencies import Input, Output;
+import pandas as pd;
+import requests;
+import plotly.express as px;
+
+
+value_vars = ["Österreich", "Burgenland", "Kärnten", "Niederösterreich", "Oberösterreich", "Salzburg", "Steiermark",
+              "Tirol", "Vorarlberg", "Wien"]
+
+def get_average_living_space_callback(app,url):
+    @app.callback(
+        Output(component_id='container', component_property='children'),
+        [Input(component_id='slct_state3', component_property='value'),
+         Input('size', 'value')]
+    )
+    def update_graph(slct_state, size):
+        container = "The predicted price is: {}".format(
+            requests.get(url + "/predictPrice?state=" + str(slct_state) + "&size=" + str(size)).json())
+
+        return container
+
+
+    @app.callback(
+        Output(component_id='living_space_line', component_property='figure'),
+        [Input(component_id='slct_state1', component_property='value')]
+    )
+    def update_graph(slct_state):
+        averageSpace = pd.read_json(requests.get(url + '/averageSpace').json())
+        df1 = averageSpace.melt(id_vars='Jahr', value_vars=value_vars).copy()
+
+        if slct_state != 'Österreich':
+            df1 = df1[df1.variable == slct_state]
+
+        fig = px.line(df1,
+                      x=df1.Jahr,
+                      y=df1.value,
+                      color=df1.variable,
+                      title="Average living space"
+                      )
+
+        fig.update_layout(
+            xaxis_title="Jahr",
+            yaxis_title="Größe",
+            legend_title="Bundesland")
+
+        return fig
+
--- frontend/main.py
+++ frontend/main.py
@@ -9,6 +9,8 @@ import plotly.express as px
 import requests
 import os
 from dash.dependencies import Input, Output, State
+from components.average_living_space.average_living_space import  average_living_space;
+from components.average_living_space.average_living_space_callback import  get_average_living_space_callback;
 
 url = os.getenv('URL')
 
@@ -33,60 +35,8 @@ app.layout = html.Div([dbc.Row(
             html.Div('Österreich Wohnsituation', className="app-header--title")
         ]
     ), style={'margin-bottom': '5px'}
-),
-    dbc.Row([
-        dbc.Col(
-            dcc.Dropdown(id="slct_state3",
-                         options=[
-                             {"label": "Burgenland", "value": 1},
-                             {"label": "Kärnten", "value": 2},
-                             {"label": "Niederösterreich", "value": 3},
-                             {"label": "Oberösterreich", "value": 4},
-                             {"label": "Salzburg", "value": 5},
-                             {"label": "Steiermark", "value": 6},
-                             {"label": "Tirol", "value": 7},
-                             {"label": "Vorarlberg", "value": 8},
-                             {"label": "Wien", "value": 9},
-                             {"label": "Österreich", "value": 10}],
-                         multi=False,
-                         value=10,
-                         style={'color': 'black'}
-                         ), width=2
-
-        ),
-        dbc.Col(
-            dcc.Dropdown(id="slct_state1",
-                         options=[
-                             {"label": "Burgenland", "value": 'Burgenland'},
-                             {"label": "Kärnten", "value": 'Kärnten'},
-                             {"label": "Niederösterreich", "value": 'Niederösterreich'},
-                             {"label": "Oberösterreich", "value": 'Oberösterreich'},
-                             {"label": "Salzburg", "value": 'Salzburg'},
-                             {"label": "Steiermark", "value": 'Steiermark'},
-                             {"label": "Tirol", "value": 'Tirol'},
-                             {"label": "Vorarlberg", "value": 'Vorarlberg'},
-                             {"label": "Wien", "value": 'Wien'},
-                             {"label": "Österreich", "value": 'Österreich'}],
-                         multi=False,
-                         value='Österreich',
-                         style={'color': 'black'}
-                         ), width=2
-        ),
-        dbc.Col(
-            daq.NumericInput(
-                id='size',
-                min=10,
-                max=500,
-                value=70,
-                style={'color': 'black'}
-            ), width=1
-        ),
-    ], style={'margin-bottom': '5px'}),
-    dbc.Row([
-        dbc.Col([html.Div(id='container', children=[])]), ], style={'margin-bottom': '5px'}),
-    dbc.Row([
-        dcc.Graph(id='living_space_line', figure={}),
-    ], style={'margin-bottom': '5px'}),
+    ),
+    average_living_space,
     dbc.Row([
         dbc.Col([
             dcc.Dropdown(id="slct_state2",
@@ -129,42 +79,7 @@ app.layout = html.Div([dbc.Row(
 ])
 
 
-@app.callback(
-    Output(component_id='container', component_property='children'),
-    [Input(component_id='slct_state3', component_property='value'),
-     Input('size', 'value')]
-)
-def update_graph(slct_state, size):
-    container = "The predicted price is: {}".format(
-        requests.get(url + "/predictPrice?state=" + str(slct_state) + "&size=" + str(size)).json())
-
-    return container
-
-
-@app.callback(
-    Output(component_id='living_space_line', component_property='figure'),
-    [Input(component_id='slct_state1', component_property='value')]
-)
-def update_graph(slct_state):
-    averageSpace = pd.read_json(requests.get(url + '/averageSpace').json())
-    df1 = averageSpace.melt(id_vars='Jahr', value_vars=value_vars).copy()
-
-    if slct_state != 'Österreich':
-        df1 = df1[df1.variable == slct_state]
-
-    fig = px.line(df1,
-                  x=df1.Jahr,
-                  y=df1.value,
-                  color=df1.variable,
-                  title="Average living space"
-                  )
-
-    fig.update_layout(
-        xaxis_title="Jahr",
-        yaxis_title="Größe",
-        legend_title="Bundesland")
-
-    return fig
+get_average_living_space_callback(app,url);
 
 
 @app.callback(
